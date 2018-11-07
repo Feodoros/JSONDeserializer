968c701f4e7a979d032273f49118e51eb0734f02
Feodoros <fzhilkin@mail.ru>
2018-11-07 22:08:57 +0300

    Добавьте файлы проекта.

---
+++ ConverterFromJson/Cities.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConverterFromJson
+{
+    //Получение информации формата JSON
+    public class Cities
+    {
+
+        public class Coord
+        {
+            public double lon { get; set; }
+            public double lat { get; set; }
+        }
+
+        public class Geoname
+        {
+            public string cl { get; set; }
+            public string code { get; set; }
+            public float parent { get; set; }
+        }
+
+        public class Lang
+        {
+            public string en { get; set; }
+            public string ru { get; set; }
+        }
+
+        public class Stat
+        {
+            public double level { get; set; }
+            public float population { get; set; }
+        }
+
+        public class Station
+        {
+            public long id { get; set; }
+            public int dist { get; set; }
+            public int kf { get; set; }
+        }
+
+        public class RootObject
+        {
+            public long id { get; set; }
+            public Coord coord { get; set; }
+            public string country { get; set; }
+            public Geoname geoname { get; set; }
+            public List<Lang> langs { get; set; }
+            public string name { get; set; }
+            public Stat stat { get; set; }
+            public List<Station> stations { get; set; }
+            public float zoom { get; set; }
+        }
+
+
+    }
+}
---
+++ ConverterFromJson/Program.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConverterFromJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string path = @"C:\Users\fzhil\Downloads\22.txt";
+            string json = "";
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            try
+            {
+                Console.WriteLine("Cчитываем весь текстовый файл формата JSON");
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    json = sr.ReadToEnd();
+                    sr.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+            //Создаем список JSON-объектов
+            List<Cities.RootObject> cities = JsonConvert.DeserializeObject<List<Cities.RootObject>>(json);
+
+            //Вытаскиваем нужные поля
+            foreach (Cities.RootObject items in cities)
+            {
+                Console.WriteLine('"' + items.name + '"' + ',');
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
